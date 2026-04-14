88d9956f7629a28bda8fc705f330ef80e8f8bb13
Guilherme Martini <guilherme.dcmartini@gmail.com>
2026-04-13 22:55:07 -0300

    json do header do site funcionando

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,43 @@
-import Web.Scotty(get, text, scotty)
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+import Web.Scotty(scotty, post, jsonData, json)
+import Data.Text (Text, unpack )
+import Data.Text.Encoding(decodeUtf8)
+import Data.CaseInsensitive(original)
+import Data.Aeson
+import GHC.Generics
+import Network.HTTP.Simple(parseRequest, httpNoBody, setRequestMethod, getResponseHeaders)
+import Network.HTTP.Types.Header(HeaderName)
+import Control.Monad.IO.Class(liftIO)
+import Data.ByteString(ByteString)
+import Data.Bifunctor
+
+
+newtype TargetURL = 
+    TargetURL {url :: Text} 
+    deriving (Show, Generic)
+
+instance FromJSON TargetURL
+
+translateHeaderByteStringToText :: [(HeaderName, ByteString)] -> [(Text, Text)]
+translateHeaderByteStringToText = map processTuple
+    where
+        processTuple = bimap (decodeUtf8 . original) decodeUtf8
 
 main :: IO ()
 main = scotty 3000 $ do
 
-    get "/analisador" $ do
-        text "Teste do analisador"
+    post "/analisador" $ do
+        receivedData <- jsonData
+
+        let linkString = unpack $ url receivedData
+
+        requestRaw <- parseRequest linkString
+
+        let request = setRequestMethod "HEAD" requestRaw
+
+        response <- liftIO $ httpNoBody request
+
+        let siteHeaders = translateHeaderByteStringToText $ getResponseHeaders response
+
+        json siteHeaders
